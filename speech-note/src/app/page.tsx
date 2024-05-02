65d0a1a1608f18d7e5e3f67dbfd11653d436c894
Adriano Silva <adrianon10@gmail.com>
2024-05-02 04:54:06 -0300

    feat: add new note card component

--- speech-note/src/app/page.tsx
+++ speech-note/src/app/page.tsx
@@ -1,3 +1,25 @@
+import { NewNoteCard } from '@/components/new-note-card'
+import { NoteCard } from '@/components/note-card'
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="m-auto my-12 max-w-6xl space-y-6">
+      <img src="/logo.svg" alt="Speech Note Logo" />
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas"
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+        />
+      </form>
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid auto-rows-[250px] grid-cols-3 gap-6">
+        <NewNoteCard />
+
+        <NoteCard />
+        <NoteCard />
+      </div>
+    </div>
+  )
 }
