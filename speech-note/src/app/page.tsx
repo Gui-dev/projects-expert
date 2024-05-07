b6da145e10ec7fc3226f7375a8d8ea81d1741df3
Adriano Silva <adrianon10@gmail.com>
2024-05-07 03:25:38 -0300

    feat: add helper function handleSearch

--- speech-note/src/app/page.tsx
+++ speech-note/src/app/page.tsx
@@ -1,13 +1,27 @@
+/* eslint-disable prettier/prettier */
 'use client'
 
+import { ChangeEvent, useEffect, useState } from 'react'
 import { v4 as uuid } from 'uuid'
 
 import { NewNoteCard } from '@/components/new-note-card'
 import { INoteProps, NoteCard } from '@/components/note-card'
-import { useState } from 'react'
 
 export default function Home() {
+  const [search, setSearch] = useState('')
   const [notes, setNotes] = useState<INoteProps[]>([])
+
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    const query = event.target.value
+    setSearch(query)
+  }
+
+  const filtered_notes =
+    search !== ''
+      ? notes.filter((note) =>
+        note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()),
+      )
+      : notes
 
   const onNoteCreated = (content: string) => {
     const new_note: INoteProps = {
@@ -16,7 +30,15 @@ export default function Home() {
       content,
     }
     setNotes((prev) => [new_note, ...prev])
+    localStorage.setItem('notes', JSON.stringify([new_note, ...notes]))
   }
+
+  useEffect(() => {
+    const notes_on_storage = localStorage.getItem('notes')
+    if (notes_on_storage) {
+      setNotes(JSON.parse(notes_on_storage))
+    }
+  }, [])
 
   return (
     <div className="m-auto my-12 max-w-6xl space-y-6">
@@ -26,12 +48,14 @@ export default function Home() {
           type="text"
           placeholder="Busque em suas notas"
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+          value={search}
+          onChange={handleSearch}
         />
       </form>
       <div className="h-px bg-slate-700" />
       <div className="grid auto-rows-[250px] grid-cols-3 gap-6">
         <NewNoteCard onNoteCreated={onNoteCreated} />
-        {notes.map((note) => {
+        {filtered_notes.map((note) => {
           return <NoteCard key={String(note.id)} note={note} />
         })}
       </div>
