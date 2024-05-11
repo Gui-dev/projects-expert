b723b73e1b2f5816819b41719448ad7fecc2995e
Adriano Silva <adrianon10@gmail.com>
2024-05-11 03:38:50 -0300

    chore: add fastify

--- poll-api/src/http/server.ts
+++ poll-api/src/http/server.ts
@@ -1,1 +1,17 @@
-import { randomUUID } from 'node:crypto'
+import Fastify from 'fastify'
+
+const app = Fastify()
+const PORT = 3333 || process.env.PORT
+
+app.get('/', () => {
+  return 'Hello World'
+})
+
+app
+  .listen({
+    port: PORT,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('❤️  HTTP server running!!!')
+  })
