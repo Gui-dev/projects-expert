0a310aefc72844a1444bff2af123e10724720f26
Adriano Silva <adrianon10@gmail.com>
2024-05-14 04:56:04 -0300

    feat: add create poll validation

---
+++ poll-api/src/modules/polls/validations/create-poll-validation.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod'
+
+export const createPollValidation = z.object({
+  title: z.string(),
+})
+
+export const createPollResponseValidation = z.object({
+  poll_id: z.string().uuid(),
+})
+
+export type CreatePollResponse = z.infer<typeof createPollResponseValidation>
