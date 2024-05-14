53b95faeceaf30b32c9077028b1da18f75d041be
Adriano Silva <adrianon10@gmail.com>
2024-05-14 04:59:33 -0300

    feat: add create table poll_options migrate

--- poll-api/src/http/server.ts
+++ poll-api/src/http/server.ts
@@ -1,11 +1,11 @@
 import Fastify from 'fastify'
 
+import { pollsRoute } from '../modules/polls/routes/polls.route'
+
 const app = Fastify()
 const PORT = 3333 || process.env.PORT
 
-app.get('/', () => {
-  return 'Hello World'
-})
+app.register(pollsRoute)
 
 app
   .listen({
@@ -13,5 +13,5 @@ app
     host: '0.0.0.0',
   })
   .then(() => {
-    console.log('❤️  HTTP server running!!!')
+    console.log(`❤️  HTTP server running!!! http://localhost:${PORT}`)
   })
