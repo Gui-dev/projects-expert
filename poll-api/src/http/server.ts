255aada6423c75eb00463f28a842dcc30a73d56d
Adriano Silva <adrianon10@gmail.com>
2024-05-16 05:15:19 -0300

    feat: add vote route POST /polls/:poll_id/votes

--- poll-api/src/http/server.ts
+++ poll-api/src/http/server.ts
@@ -1,13 +1,21 @@
 import Fastify from 'fastify'
+import cookie from '@fastify/cookie'
 
 import { createPollsRoute } from '../modules/polls/routes/create-polls.route'
 import { getPollsRoute } from '../modules/polls/routes/get-polls.route'
+import { voteOnPollsRoute } from '../modules/polls/routes/vote-on-polls.route'
 
 const app = Fastify()
 const PORT = 3333 || process.env.PORT
 
+app.register(cookie, {
+  secret: process.env.COOKIE_SECRET,
+  hook: 'onRequest',
+})
+
 app.register(createPollsRoute)
 app.register(getPollsRoute)
+app.register(voteOnPollsRoute)
 
 app
   .listen({
---
+++ poll-api/src/modules/polls/routes/vote-on-polls.route.ts
@@ -0,0 +1,41 @@
+import { type FastifyReply, type FastifyInstance } from 'fastify'
+import { randomUUID } from 'node:crypto'
+
+import {
+  voteOnPollParamsValidation,
+  voteOnPollValidation,
+} from './../../votes/validations/vote-on-poll-validation'
+import { CreateVote } from '../../votes/use-cases/create-vote'
+
+export const voteOnPollsRoute = async (app: FastifyInstance): Promise<void> => {
+  app.post(
+    '/polls/:poll_id/votes',
+    async (request, reply): Promise<FastifyReply> => {
+      const { poll_id } = voteOnPollParamsValidation.parse(request.params)
+      const { poll_option_id } = voteOnPollValidation.parse(request.body)
+      const createVote = new CreateVote()
+      let { session_id } = request.cookies
+
+      console.log('SessionID: ', session_id)
+
+      if (!session_id) {
+        session_id = randomUUID()
+
+        reply.setCookie('session_id', session_id, {
+          path: '/',
+          maxAge: 60 * 60 * 24 * 30, // 30 days
+          signed: true,
+          httpOnly: true,
+        })
+      }
+
+      const vote = await createVote.execute({
+        poll_id,
+        poll_option_id,
+        session_id,
+      })
+
+      return reply.status(201).send({ vote })
+    },
+  )
+}
