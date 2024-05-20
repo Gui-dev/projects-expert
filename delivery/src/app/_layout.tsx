6ac4236e4c4bc7671431ef7366d813283341fb1e
Adriano Silva <adrianon10@gmail.com>
2024-05-20 18:02:43 -0300

    feat: add font google Inter

--- delivery/src/app/_layout.tsx
+++ delivery/src/app/_layout.tsx
@@ -1,14 +1,38 @@
+import { SafeAreaView, View } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import { Slot } from 'expo-router'
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  useFonts,
+} from '@expo-google-fonts/inter'
 
-import '@/src/assets/styles/globals.css'
+import '@/assets/styles/globals.css'
+import { Loading } from '@/components/loading'
 
 const Layout = () => {
+  const [isFontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+  })
+
+  if (!isFontsLoaded) {
+    return (
+      <View className="flex-1 items-center justify-center bg-slate-900">
+        <Loading />
+      </View>
+    )
+  }
+
   return (
-    <>
+    <SafeAreaView className="flex-1 bg-slate-900">
       <StatusBar style="light" translucent />
       <Slot />
-    </>
+    </SafeAreaView>
   )
 }
 
--- delivery/src/app/index.tsx
+++ delivery/src/app/index.tsx
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native'
 
 const Home = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-zinc-950">
+    <View className="flex-1 items-center justify-center">
       <Text className="text-2xl text-zinc-100">Hello World</Text>
     </View>
   )
